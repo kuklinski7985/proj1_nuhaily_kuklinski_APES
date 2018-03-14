8a8ec337659e63d94a707125036122b25df94770
anuhaily <adam.nuhaily@colorado.edu>
2018-03-13 18:39:59 -0600

    Resolving merge again

---
+++ main.h
@@ -0,0 +1,30 @@
+/**
+* @file main.h
+* @brief main fxn for project1 - APES
+* @author Andrew Kuklinski and Adam Nuhaily
+* @date 03/11/2018
+**/
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <mqueue.h>
+#include "i2c_wrapper.h"
+#include "tempsense.h"
+#include "temp_ops.h"
+#include "light_ops.h"
+#include "remote_socket.h"
+#include "logger/logger.h"
+//#include "myusrled.h"
+
+typedef struct input_struct{
+  int member1;
+} input_struct;
